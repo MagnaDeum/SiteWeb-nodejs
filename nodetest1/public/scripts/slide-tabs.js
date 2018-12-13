5dc081ef2720fe2284930230efe7658e4496e914
max <max@MOUSE.matrix.onra.org>
2018-12-13 16:25:29 +0100

    Menu tabs correctly sliding.

--- nodetest1/public/scripts/slide-tabs.js
+++ nodetest1/public/scripts/slide-tabs.js
@@ -1,21 +1,22 @@
+
+
 // slide effect on menu tabs
 $(".tab-name").on({
     mouseenter: function(){
-        console.log("testtab")
-        $(this).next(".slide-tab").animate({
+        $(this).next(".slide-tab").stop().animate({
             opacity: "1",
             right: "0",
             },
-            250,
+            200,
             function(){}
         );
     },
     mouseleave: function(){
-        $(this).next(".slide-tab").animate({
+        $(this).next(".slide-tab").stop().animate({
             opacity: "0",
             right: "100%",
             },
-            250,
+            100,
             function(){}
         );
     }
@@ -24,10 +25,8 @@ $(".tab-name").on({
 // slide effect on drop down menu
 $("#hobbies").on({
     mouseenter: function(){
-        console.log("test")
-        $("#hobbies").css("background", "none"); 
         $("#hobbies-menu").css("display", "block");
-        $("#hobbies-menu").animate({
+        $("#hobbies-menu").stop().animate({
             opacity: "1",
             marginLeft: "0",
             },
@@ -36,15 +35,31 @@ $("#hobbies").on({
         );
     },
     mouseleave: function(){
-        $("#hobbies-menu").animate({
+        $("#hobbies-menu").stop().animate({
             opacity: "0",
             marginLeft: "-20%",
             },
             250,
             function(){ 
                 $("#hobbies-menu").css("display", "none"); 
-                $("#hobbies").css("background", "rgb(31, 72, 81)"); // has to be the value of $background sass variable
             }
         );
     }
 });
+
+$("#hobbies-menu").on({
+    mouseenter: function(){
+        $("#hobbies .tab-name").first().next(".slide-tab").stop().css("opacity","1");
+        $("#hobbies .tab-name").first().next(".slide-tab").stop().css("right","0");
+    },
+    mouseleave: function(){
+        $("#hobbies .tab-name").first().next(".slide-tab").stop().animate({
+            opacity: "0",
+            right: "100%",
+            },
+            100,
+            function(){}
+        );
+    }
+});
+
