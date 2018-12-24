164c6c0ded43e62147c1a63a00e2015ab073d8e4
MagnaDeum <maxime.leclerc.insa@gmail.com>
2018-12-24 14:18:47 +0100

    Changed some desc text. Fixed audio player on play button not playing.

--- nodetest1/public/scripts/mp3player.js
+++ nodetest1/public/scripts/mp3player.js
@@ -1,43 +1,6 @@
-
-// jquery audio event listener patching
-
-jQuery.createEventCapturing = (function () {
-    var special = jQuery.event.special;
-    return function (names) {
-        if (!document.addEventListener) {
-            return;
-        }
-        if (typeof names == 'string') {
-            names = [names];
-        }
-        jQuery.each(names, function (i, name) {
-            var handler = function (e) {
-                e = jQuery.event.fix(e);
-
-                return jQuery.event.dispatch.call(this, e);
-            };
-            special[name] = special[name] || {};
-            if (special[name].setup || special[name].teardown) {
-                return;
-            }
-            jQuery.extend(special[name], {
-                setup: function () {
-                    this.addEventListener(name, handler, true);
-                },
-                teardown: function () {
-                    this.removeEventListener(name, handler, true);
-                }
-            });
-        });
-    };
-})();
-
-jQuery.createEventCapturing(['play']);  
-
-
 // only one player can play at the same time
 
-$("#music-list").on("play", ".mp3player audio", function(){
+$("#music-list").on("click", ".mp3player audio", function(){
     var _this = $(this);
     $("audio").each(function(i,el){
         if(!$(el).is(_this)){
@@ -48,9 +11,15 @@ $("#music-list").on("play", ".mp3player audio", function(){
 
 // user can click row to play the music
 
-$("#music-list").on("click", ".mp3player", function(){
-    var player = $(this).children("audio");
+$("#music-list").on("click", ".musicName", function(){
+    var player = $(this).next("audio");
     if(player.get(0).paused){
+        // stop other players
+        $("audio").each(function(i,el){
+            if(!$(el).is(player)){
+                $(el).get(0).pause();
+            }
+        });
         player.get(0).play();
     }else{
         player.get(0).pause();
